10acd444a9a5426f0976fb62a9f2982fb5437fa5
Gerben <info@gerbenvaneerten.nl>
2023-12-04 03:39:41 +0100

    Rewrote into non blocking measurements, implemented chip configuration, refactored code

---
+++ examples/Example/DSB18B20.ino
@@ -0,0 +1,102 @@
+#include "DS18B20.h"
+#include "Return.h"
+
+using namespace g3rb3n;
+
+#define RESOLUTION 9
+
+DS18B20 sensor(D2);
+
+float value;
+
+String chip(uint8_t family)
+{
+    switch (family)
+    {
+    case 0x10:
+        return String("DS18S20"); // or old DS1820
+    case 0x28:
+        return String("DS18B20");
+    case 0x22:
+        return String("DS1822");
+    }
+    return String("UNKNOWN");
+}
+
+void setup()
+{
+    Serial.begin(230400);
+    Serial.println();
+    uint8_t code = sensor.begin();
+    if (code > 0)
+        Serial.println(String("Error ") + code);
+    Serial.println("Found chip family " + chip(sensor.family()));
+    Serial.println("Serial " + sensor.serial());
+    Serial.println("Resolution " + String(sensor.resolution()));
+    Serial.println("Conversion time " + String(sensor.measuringTimeMillis()) + "ms");
+    Serial.println();
+
+    sensor.resolution(RESOLUTION);
+    Serial.println("Resolution " + String(sensor.resolution()));
+}
+
+void loop()
+{
+    sensor.handle();
+    if (sensor.measuring())
+        return;
+    nonBlockingMeasurementEnd();
+    handleInput();
+    nonBlockingMeasurementStart();
+}
+
+void handleInput()
+{
+    if (Serial.available())
+    {
+        char c = Serial.read();
+        Serial.print("Got ");
+        Serial.println(c);
+        if (c < 49 || c > 52)
+        {
+            Serial.println("!!! Wrong command");
+            Serial.println("!!! 1 for 9 bit resolution");
+            Serial.println("!!! 2 for 10 bit resolution");
+            Serial.println("!!! 3 for 11 bit resolution");
+            Serial.println("!!! 4 for 12 bit resolution");
+            return;
+        }
+        uint8_t resolution = c - 49 + 9;
+        Serial.print("Set ");
+        Serial.println(resolution);
+        sensor.resolution(resolution);
+        Serial.println("Resolution " + String(sensor.resolution()));
+    }
+}
+
+void blockingMeasurement()
+{
+    Return<float> temperature = sensor.temperature();
+    if (!temperature.valid())
+    {
+        Serial.println(String("Error ") + temperature.code());
+        return;
+    }
+    Serial.println(String("Read ") + static_cast<float>(temperature));
+}
+
+void nonBlockingMeasurementEnd()
+{
+    Return<float> temperature = sensor.lastTemperature();
+    if (!temperature.valid())
+    {
+        Serial.println(String("Error ") + temperature.code());
+        return;
+    }
+    Serial.println(String(millis()) + String(" read ") + static_cast<float>(temperature));
+}
+
+void nonBlockingMeasurementStart()
+{
+    sensor.startMeasurement();
+}
--- src/DS18B20.cpp
+++ src/DS18B20.cpp
@@ -2,141 +2,365 @@
 
 using namespace g3rb3n;
 
-DS18B20::DS18B20(uint8_t _pin)
-:
-wire(new OneWire(_pin))
+DS18B20::DS18B20(uint8_t _pin, bool parasitic)
+    : wire(new OneWire(_pin)),
+      parasitic(parasitic)
 {
-  
 }
 
-bool DS18B20::begin()
+uint8_t DS18B20::begin()
 {
-  int count = 0;
-  byte addr[8];
+    startErrorCode = 0;
 
-  while ( !wire->search(addr)) 
-  {
-    wire->reset_search();
-    delay(250);
-    if (count++ >= 10)
+    int count = 0;
+    while (!wire->search(rom))
     {
-      return false;
+        wire->reset_search();
+        delay(250);
+        yield();
+        if (count++ >= 10)
+        {
+#ifdef DS18B20_DEBUG
+            Serial.println("Did not find DS18B20");
+#endif
+            startErrorCode = DS18B20_ERROR_NOT_FOUND;
+            return startErrorCode;
+        }
     }
-  }
-  
-  if (OneWire::crc8(addr, 7) != addr[7]) 
-  {
-    return false;
-  }
 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10: return true;
-    case 0x28: return true;
-    case 0x22: return true;
-    default: return false;
-  } 
-  return true;
+#ifdef DS18B20_DEBUG
+    Serial.print("ROM = ");
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        Serial.write(' ');
+        Serial.print(rom[i], HEX);
+    }
+    Serial.println();
+    Serial.print("Family = ");
+    Serial.print(rom[0], HEX);
+    Serial.println();
+    Serial.print("Serial = ");
+    for (uint8_t i = 1; i < 7; i++)
+    {
+        Serial.write(' ');
+        Serial.print(rom[i], HEX);
+    }
+    Serial.println();
+#endif
+
+    if (OneWire::crc8(rom, 7) != rom[7])
+    {
+#ifdef DS18B20_DEBUG
+        Serial.println("ROM CRC is not valid!");
+#endif
+        startErrorCode = DS18B20_ERROR_ADDRESS_CRC;
+        return startErrorCode;
+    }
+
+    // the first ROM byte indicates which chip
+    switch (rom[0])
+    {
+    case 0x10:
+        //      Serial.println("  Chip = DS18S20");  // or old DS1820
+        type_s = 1;
+        break;
+    case 0x28:
+        //      Serial.println("  Chip = DS18B20");
+        type_s = 0;
+        break;
+    case 0x22:
+        //      Serial.println("  Chip = DS1822");
+        type_s = 0;
+        break;
+    default:
+#ifdef DS18B20_DEBUG
+        Serial.println("Device is not a DS18x20 family device.");
+#endif
+        startErrorCode = DS18B20_ERROR_CHIP_NOT_SUPPORTED;
+        return startErrorCode;
+    }
+    startErrorCode = readScratchpad();
+    if (startErrorCode != 0)
+        return startErrorCode;
+    return startErrorCode;
 }
 
+// Reads the entire scratchpad including the CRC byte.
+uint8_t DS18B20::readScratchpad()
+{
+    uint8_t present = 0;
+
+    present = wire->reset();
+    if (!present)
+    {
+        Serial.println("Sensor not present");
+        return DS18B20_ERROR_NOT_PRESENT;
+    }
+    wire->select(rom);
+    wire->write(DS18B20_COMMAND_READ_SCRATCHPAD); // Read Scratchpad
+
+#ifdef DS18B20_DEBUG
+    Serial.print("  Data = ");
+    Serial.print(present, HEX);
+    Serial.print(" ");
+#endif
+    for (uint8_t i = 0; i < 9; i++)
+    { // we need 9 bytes
+        data[i] = wire->read();
+#ifdef DS18B20_DEBUG
+        Serial.print(data[i], HEX);
+        Serial.print(" ");
+#endif
+    }
+
+#ifdef DS18B20_DEBUG
+    Serial.print(" CRC=");
+    Serial.print(OneWire::crc8(data, 8), HEX);
+    Serial.println();
+#endif
+
+    if (OneWire::crc8(data, 8) != data[8])
+    {
+#ifdef DS18B20_DEBUG
+        Serial.println("CRC is not valid!");
+#endif
+        return DS18B20_ERROR_DATA_CRC;
+    }
+    return 0;
+}
+
+uint8_t DS18B20::configuration()
+{
+    return data[DS18B20_SCRATCHPAD_BYTE_CONFIGURATION_REGISTER];
+}
+
+uint8_t DS18B20::family()
+{
+    return rom[DS18B20_ROM_BYTE_FAMILY];
+}
+
+String DS18B20::serial()
+{
+    String ret;
+    for (uint8_t i = DS18B20_ROM_BYTE_SERIAL_START; i <= DS18B20_ROM_BYTE_SERIAL_END; ++i)
+        ret += String(rom[i], HEX);
+    return ret;
+}
+
+uint8_t DS18B20::resolution()
+{
+    if (type_s)
+        return 9;
+    uint8_t cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00)
+        return 9;
+    if (cfg == 0x20)
+        return 10;
+    if (cfg == 0x40)
+        return 11;
+    return 12;
+}
+
+uint8_t DS18B20::resolution(uint8_t resolution)
+{
+    uint8_t cfg;
+    switch (resolution)
+    {
+    case 9:
+        cfg = 0x00;
+        break;
+    case 10:
+        cfg = 0x20;
+        break;
+    case 11:
+        cfg = 0x40;
+        break;
+    case 12:
+        cfg = 0x60;
+        break;
+    default:
+        return 255;
+    }
+    data[DS18B20_SCRATCHPAD_BYTE_CONFIGURATION_REGISTER] &= 0x0F;
+    data[DS18B20_SCRATCHPAD_BYTE_CONFIGURATION_REGISTER] |= cfg;
+#ifdef DS18B20_DEBUG
+    Serial.println(data[DS18B20_SCRATCHPAD_BYTE_CONFIGURATION_REGISTER]);
+#endif
+    return writeScratchpad();
+}
+
+uint16_t DS18B20::measuringTimeMillis()
+{
+    uint8_t res = resolution();
+    switch (res)
+    {
+    case 9:
+        return 94; // 9 bit resolution, 93.75 ms
+    case 10:
+        return 188; // 10 bit res, 187.5 ms
+    case 11:
+        return 375; // 11 bit res, 375 ms
+    default:
+        return 750; // Default is 12 bit resolution, 750 ms conversion time
+    }
+    return 750;
+}
+
+// Writes data into scratchpad bytes 2, 3, and 4 (T H , T L , and configuration registers).
+uint8_t DS18B20::writeScratchpad()
+{
+    wire->reset();
+    wire->select(rom);
+    wire->write(DS18B20_COMMAND_WRITE_SCRATCHPAD, parasitic);
+    wire->write_bytes(data + DS18B20_SCRATCHPAD_WRITEABLE_START, DS18B20_SCRATCHPAD_WRITEABLE_LENGTH, parasitic);
+    wire->reset();
+    readScratchpad();
+    return 0;
+}
+
+// Copies T H , T L , and configuration register data from the scratchpad to EEPROM.
+uint8_t DS18B20::saveScratchpad()
+{
+    return 0;
+}
+
+// Recalls T H , T L , and configuration register data from EEPROM to the scratchpad.
+uint8_t DS18B20::loadScratchpad()
+{
+    return 0;
+}
 
 Return<float> DS18B20::temperature()
 {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
-  int count = 0;
-  while ( !wire->search(addr)) {
-//    Serial.println("No more addresses.");
-//    Serial.println();
-    wire->reset_search();
-    delay(250);
-    if (count++ >= 10)
-      return Return<float>::error(1);
-  }
-  
-//  Serial.print("ROM =");
-//  for( i = 0; i < 8; i++) {
-//    Serial.write(' ');
-//    Serial.print(addr[i], HEX);
-//  }
+    uint8_t i;
+    float celsius;
 
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return Return<float>::error(2);
-  }
-//  Serial.println();
- 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-//      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-//      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-//      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return Return<float>::error(3);
-  } 
-
-  wire->reset();
-  wire->select(addr);
-  wire->write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a wire->depower() here, but the reset will take care of it.
-  
-  present = wire->reset();
-  wire->select(addr);    
-  wire->write(0xBE);         // Read Scratchpad
-
-//  Serial.print("  Data = ");
-//  Serial.print(present, HEX);
-//  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = wire->read();
-//    Serial.print(data[i], HEX);
-//    Serial.print(" ");
-  }
-//  Serial.print(" CRC=");
-//  Serial.print(OneWire::crc8(data, 8), HEX);
-//  Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
+    if (startErrorCode != 0)
+    {
+        begin();
+        return Return<float>::error(startErrorCode);
     }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-//  Serial.print("  Temperature = ");
-//  Serial.print(celsius);
-//  Serial.print(" Celsius, ");
-//  Serial.print(fahrenheit);
-//  Serial.println(" Fahrenheit");
-  return celsius;
+
+    wire->reset();
+    wire->select(rom);
+    wire->write(DS18B20_COMMAND_CONVERT_TEMPERATURE, parasitic); // start conversion, with parasite power on at the end
+
+    delay(measuringTimeMillis()); // Use the documented timing
+
+    // we might do a wire->depower() here, but the reset will take care of it.
+    uint8_t code = readScratchpad();
+    if (code != 0)
+        return Return<float>::error(code);
+
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s)
+    {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10)
+        {
+            // "count remain" gives full 12 bit resolution
+            raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+    }
+    else
+    {
+        uint8_t cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00)
+            raw = raw & ~7; // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20)
+            raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40)
+            raw = raw & ~1; // 11 bit res, 375 ms
+                            // Default is 12 bit resolution, 750 ms conversion time
+    }
+    celsius = (float)raw / 16.0;
+#ifdef DS18B20_DEBUG
+    Serial.print("  Temperature = ");
+    Serial.print(celsius);
+    Serial.print(" Celsius, ");
+    Serial.print(celsius * 1.8 + 32.0);
+    Serial.println(" Fahrenheit");
+#endif
+    return celsius;
+}
+
+Return<float> DS18B20::lastTemperature()
+{
+    float celsius;
+    // we might do a wire->depower() here, but the reset will take care of it.
+    uint8_t code = readScratchpad();
+    if (code != 0)
+        return Return<float>::error(code);
+
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s)
+    {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10)
+        {
+            // "count remain" gives full 12 bit resolution
+            raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+    }
+    else
+    {
+        uint8_t cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00)
+            raw = raw & ~7; // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20)
+            raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40)
+            raw = raw & ~1; // 11 bit res, 375 ms
+                            // Default is 12 bit resolution, 750 ms conversion time
+    }
+    celsius = (float)raw / 16.0;
+#ifdef DS18B20_DEBUG
+    Serial.print("  Temperature = ");
+    Serial.print(celsius);
+    Serial.print(" Celsius, ");
+    Serial.print(celsius * 1.8 + 32.0);
+    Serial.println(" Fahrenheit");
+#endif
+    return celsius;
+}
+
+uint8_t DS18B20::startMeasurement()
+{
+
+    if (startErrorCode != 0)
+    {
+        begin();
+        return startErrorCode;
+    }
+    wire->reset();
+    wire->select(rom);
+    wire->write(DS18B20_COMMAND_CONVERT_TEMPERATURE, parasitic); // start conversion, with parasite power on at the end
+    _measuring = true;
+    measurementStarted = millis();
+    return 0;
+}
+
+bool DS18B20::measuring()
+{
+    return _measuring;
+}
+
+bool DS18B20::handle()
+{
+    if (_measuring && millis() - measurementStarted > measuringTimeMillis())
+    {
+        _measuring = false;
+    }
+    return true;
 }
